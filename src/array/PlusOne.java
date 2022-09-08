2c40ba78c58ebb3f9be44f5ea1cbd14dd83974c4
guanjingdan <guanjingdan@apusapps.com>
2022-09-08 15:12:22 +0800

    加一

---
+++ src/array/PlusOne.java
@@ -0,0 +1,18 @@
+package array;
+
+/**
+ * @ClassName PlusOneNew
+ * @Description You are given a large integer represented as an integer array digits,
+ * where each digits[i] is the ith digit of the integer.
+ * The digits are ordered from most significant to least significant in left-to-right order.
+ * The large integer does not contain any leading 0's.
+ * Increment the large integer by one and return the resulting array of digits.
+ * e.g.
+ * 1. Input: digits = [1,2,3] Output: [1,2,4]
+ * 2. Input: digits = [9] Output: [1,0]
+ * @Author GJD
+ * @Date 2022/9/8 15:06
+ **/
+public class PlusOne {
+
+}
