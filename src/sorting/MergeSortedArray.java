c3d51e92e69717013ab976b639d816aab0d194ce
guanjingdan <jingdanguan@163.com>
2022-09-22 20:28:02 +0800

    88 合并有序数组

--- src/sorting/MergeSortedArray.java
+++ src/sorting/MergeSortedArray.java
@@ -20,6 +20,21 @@ public class MergeSortedArray {
         mergeSortedArray(nums1, 3, nums2, 3);
     }
     public static void mergeSortedArray(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m-1;
+        int p2 = n-1;
+        for (int i = nums1.length -1; i >= 0; i--) {
+            if (p1 == -1) {
+                nums1[i] = nums2[p2--];
+            } else if (p2 == -1 || nums1[p1] > nums2[p2]) {
+                nums1[i] = nums1[p1--];
+            } else {
+                nums1[i] = nums2[p2--];
+            }
+        }
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public static void mergeSortedArrayV2(int[] nums1, int m, int[] nums2, int n) {
         int p1 = m - 1, p2 = n - 1;
         int tail = m + n - 1;
         int cur;
